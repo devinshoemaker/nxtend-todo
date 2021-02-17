197f1dcd75a8177e78c54113c6fa7d9a82c6cd00
Devin Shoemaker <devinshoe@gmail.com>
2021-02-16 18:11:27 -0600

    Initialize Firebase (#13)

---
+++ apps/todo/src/firebase/firebase-config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBIW5X6AVH-yoWvLCEM5xE754-zYChHGfo',
+  authDomain: 'nxtend-todo.firebaseapp.com',
+  projectId: 'nxtend-todo',
+  storageBucket: 'nxtend-todo.appspot.com',
+  messagingSenderId: '80714926163',
+  appId: '1:80714926163:web:2710158d8889cac3fa5ea6',
+  measurementId: 'G-3SXWJP5NTZ',
+};
+
+export default firebaseConfig;
